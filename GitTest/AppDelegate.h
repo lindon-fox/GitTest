9115f54b8edc899c33be25bd2c7b2cada6811398
John Grattan <john.grattan@ienglish.com>
2013-01-08 10:02:21 +0900

    Initial Commit

---
+++ GitTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GitTest
+//
+//  Created by John Grattan on 13/8/1.
+//  Copyright (c) 2013 John Grattan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
